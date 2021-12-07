123d96638a7d229e745903d91a536f38aec5013d
Dmitry Fokin <fokind1@yandex.ru>
2021-12-07 12:13:56 +0300

    init

---
+++ src/test/initMockServer.ts
@@ -0,0 +1,6 @@
+import mockserver from "../localService/mockserver";
+// initialize the mock server
+mockserver.init();
+
+// initialize the embedded component on the HTML page
+sap.ui.require(["sap/ui/core/ComponentSupport"]);
